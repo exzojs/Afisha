4ef691a5311ee1ec7d291bfcf00cc3cf63094bf7
Серков Евгений <exzojs@gmail.com>
2022-08-30 22:31:31 +0300

    addedTests

--- src/test/java/ru/netology/afishamanager/AfishaMckt/managertest/AfishaTest.java
+++ src/test/java/ru/netology/afishamanager/AfishaMckt/managertest/AfishaTest.java
@@ -22,11 +22,12 @@ public class AfishaTest {
         afisha.addedFilm("Film10");
 
         String[] actual = afisha.findAll();
-        String[] expexted = {"Film1","Film2","Film3","Film4","Film5","Film6","Film7","Film8","Film9","Film10"};
+        String[] expexted = {"Film1", "Film2", "Film3", "Film4", "Film5", "Film6", "Film7", "Film8", "Film9", "Film10"};
         Assertions.assertArrayEquals(expexted, actual);
     }
+
     @Test
-    public void findLastFilm(){
+    public void findLastFilm() {
         afisha.addedFilm("Film1");
         afisha.addedFilm("Film2");
         afisha.addedFilm("Film3");
@@ -39,10 +40,11 @@ public class AfishaTest {
         afisha.addedFilm("Film10");
 
         String[] actual = afisha.findLast();
-        String[] expexted = {"Film10","Film9","Film8","Film7","Film6","Film5","Film4","Film3","Film2","Film1"};
+        String[] expexted = {"Film10", "Film9", "Film8", "Film7", "Film6", "Film5", "Film4", "Film3", "Film2", "Film1"};
         Assertions.assertArrayEquals(expexted, actual);
 
     }
+
     @Test
     public void lastFilmBelowLimit() {
         afisha.addedFilm("Film1");
@@ -56,8 +58,46 @@ public class AfishaTest {
         afisha.addedFilm("Film9");
 
         String[] actual = afisha.findLast();
-        String[] expexted = {"Film9","Film8","Film7","Film6","Film5","Film4","Film3","Film2","Film1"};
+        String[] expexted = {"Film9", "Film8", "Film7", "Film6", "Film5", "Film4", "Film3", "Film2", "Film1"};
         Assertions.assertArrayEquals(expexted, actual);
 
+    }
+
+    @Test
+    public void lastFilmOnLimit() {
+        afisha.addedFilm("Film1");
+        afisha.addedFilm("Film2");
+        afisha.addedFilm("Film3");
+        afisha.addedFilm("Film4");
+        afisha.addedFilm("Film5");
+        afisha.addedFilm("Film6");
+        afisha.addedFilm("Film7");
+        afisha.addedFilm("Film8");
+        afisha.addedFilm("Film9");
+        afisha.addedFilm("Film10");
+
+        String[] actual = afisha.findLast();
+        String[] expexted = {"Film10", "Film9", "Film8", "Film7", "Film6", "Film5", "Film4", "Film3", "Film2", "Film1"};
+        Assertions.assertArrayEquals(expexted, actual);
+    }
+
+    @Test
+    public void lastFilmOverLimit() {
+        afisha.addedFilm("Film1");
+        afisha.addedFilm("Film2");
+        afisha.addedFilm("Film3");
+        afisha.addedFilm("Film4");
+        afisha.addedFilm("Film5");
+        afisha.addedFilm("Film6");
+        afisha.addedFilm("Film7");
+        afisha.addedFilm("Film8");
+        afisha.addedFilm("Film9");
+        afisha.addedFilm("Film10");
+        afisha.addedFilm("Film11");
+
+
+        String[] actual = afisha.findLast();
+        String[] expexted = {"Film11", "Film10", "Film9", "Film8", "Film7", "Film6", "Film5", "Film4", "Film3", "Film2",};
+        Assertions.assertArrayEquals(expexted, actual);
     }
 }
